87be1c9f65c53e039abd859fba0d6e30aae9e26e
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-02 22:12:26 +0000

    func mx_strstr

---
+++ sprint04/t10/mx_strstr.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+char *mx_strstr(const char *s1, const char *s2) {
+    for (;s1 && *s1; ++s1) {
+        char *s_copy = (char*)s1;
+
+        for (;s2 && *s_copy == *s2; ++s_copy, ++s2) {
+            if (!*(s2 + 1))
+                return (char*)s1;
+        }
+        s2 -= (s_copy - s1);
+    }
+    return NULL;
+}
+
+// #include <string.h>
+
+// int main(void) {
+//     printf("%s\n", mx_strstr("bbbbbbbs", "s"));
+//     printf("%s\n\n", strstr("bbbbbbbs", "s"));
+    
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "s"));
+//     printf("%s\n\n", strstr("strsrrstr", "s"));
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "st"));
+//     printf("%s\n\n", strstr("strsrrstr", "st"));
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "str"));
+//     printf("%s\n\n", strstr("strsrrstr", "str"));
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "strsrrstr"));
+//     printf("%s\n\n", strstr("strsrrstr", "strsrrstr"));
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "t"));
+//     printf("%s\n\n", strstr("strsrrstr", "t"));
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "r"));
+//     printf("%s\n\n", strstr("strsrrstr", "r"));
+    
+//     printf("%s\n", mx_strstr("strsrrstr", "srr"));
+//     printf("%s\n\n", strstr("strsrrstr", "srr"));
+// }
