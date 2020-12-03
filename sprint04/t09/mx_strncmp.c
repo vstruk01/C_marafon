1156c20faf794b5f53e26c807b840cd2a09c7888
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-03 14:53:48 +0000

    fix func mx_strncmp

--- sprint04/t09/mx_strncmp.c
+++ sprint04/t09/mx_strncmp.c
@@ -1,5 +1,5 @@
 int mx_strncmp(const char *s1, const char *s2, int n) {
-    for (;*s1 != '\0' && *s2 != '\0' && n > 0 && *s1 == *s2; n--, s1++, s2++);
+    for (;*s1 != '\0' && *s2 != '\0' && n - 1 > 0 && *s1 == *s2; --n, ++s1, ++s2);
     return *s1 == *s2 ? 0 : *s1 > *s2 ? 1 : -1;
 }
 
@@ -9,7 +9,7 @@ int mx_strncmp(const char *s1, const char *s2, int n) {
 //     printf("expected =  0, result =  %d\n", mx_strncmp("vlad", "vlad", 4));
 //     printf("expected = -1, result = %d\n", mx_strncmp("alad", "flad", 1));
 //     printf("expected = -1, result = %d\n", mx_strncmp("alad", "blad", 4));
-//     printf("expected =  0, result =  z%d\n", mx_strncmp("vldd", "vlad", 2));
+//     printf("expected =  0, result =  %d\n", mx_strncmp("vldd", "vlad", 2));
 //     printf("expected =  1, result =  %d\n", mx_strncmp("blad", "alad", 4));
 //     printf("expected = -1, result = %d\n", mx_strncmp("vlad", "vlbd", 3));
 // }
