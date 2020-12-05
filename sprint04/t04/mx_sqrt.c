f5adba4e79ac51242c1e3b69ad0c955db1c572ab
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-05 19:33:25 +0000

    fix mx_sqrt

--- sprint04/t04/mx_sqrt.c
+++ sprint04/t04/mx_sqrt.c
@@ -7,23 +7,32 @@ int mx_sqrt(int x) {
     float last_result = 0;
     int i = 0;
     
-    while (result * result != x) {
-        if (last_result - result < 1 && last_result - result > -1) {
-            if ((int)result * (int)result != x)
-                result = 0;
+    while (result > 0 && result * result != x) {
+        if (last_result - result < 1 && last_result - result > -1)
             break;
-        }
         last_result = result;
         result = (result + x / result) / 2;
         // printf("i = %d\tval = %0.2f\n", i, result);
         ++i;
     }
-    return result;
+    return (int)result * (int)result != x ? 0 : (int)result;
 }
 
 // #include <stdio.h>
 
 // int main(void) {
+//     int res = 0;
+//     for (int i = 0; i < 2147483647; ++i) {
+//         res = mx_sqrt(i);
+//         if (res) {
+//             printf("i = %d --- %d\n", i, res);    
+//         }
+//     }
+    
+//     printf("%d\n", mx_sqrt(-2345));
+    
+//     printf("%d\n", 1546 * 1546);
+//     printf("%d\n", mx_sqrt(2392576));
 //     printf("%d\n", mx_sqrt(0));
 //     printf("%d\n", mx_sqrt(1));
 //     printf("%d\n", mx_sqrt(2));
