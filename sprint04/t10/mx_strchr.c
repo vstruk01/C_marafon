bbd78efbd6c239daf871c1a58737138bba8749de
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-05 19:34:24 +0000

    mx_strchr

--- sprint04/t10/mx_strchr.c
+++ sprint04/t10/mx_strchr.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 
 char *mx_strchr(const char *s, int c) {
-    for (;*s; s++) {
+    for (;*s; ++s) {
         if (*s == c)
             return (char *)s;
     }
+    if (*s == c)
+        return (char *)s;
     return NULL;
 }
 
 // #include <string.h>
 
 // int main() {
-//     char *s = "saaabbbcccdddfffgggeee";
-//     printf("%s\n", mx_strchr(s, 's'));
-//     printf("%s\n\n", strchr(s, 's'));
+    // char *s = "saaabbbC cccdddfffgggeee";
+    // printf("%s\n", mx_strchr(s, 's'));
+    // printf("%s\n\n", strchr(s, 's'));
     
-//     printf("%s\n", mx_strchr(s, 'a'));
-//     printf("%s\n\n", strchr(s, 'a'));
+    // printf("%s\n", mx_strchr(s, 'a'));
+    // printf("%s\n\n", strchr(s, 'a'));
     
-//     printf("%s\n", mx_strchr(s, 'b'));
-//     printf("%s\n\n", strchr(s, 'b'));
+    // printf("%s\n", mx_strchr(s, 'b'));
+    // printf("%s\n\n", strchr(s, 'b'));
     
-//     printf("%s\n", mx_strchr(s, 'c'));
-//     printf("%s\n\n", strchr(s, 'c'));
+    // printf("%s\n", mx_strchr(s, 'c'));
+    // printf("%s\n\n", strchr(s, 'c'));
     
-//     printf("%s\n", mx_strchr(s, 'f'));
-//     printf("%s\n\n", strchr(s, 'f'));
+    // printf("%s\n", mx_strchr(s, 'f'));
+    // printf("%s\n\n", strchr(s, 'f'));
     
-//     printf("%s\n", mx_strchr(s, 'g'));
-//     printf("%s\n\n", strchr(s, 'g'));
+    // printf("%s\n", mx_strchr(s, 'g'));
+    // printf("%s\n\n", strchr(s, 'g'));
     
-//     printf("%s\n", mx_strchr(s, 'e'));
-//     printf("%s\n\n", strchr(s, 'e'));
+    // printf("%s\n", mx_strchr(s, 'e'));
+    // printf("%s\n\n", strchr(s, 'e'));
     
+    // printf("%s\n", mx_strchr(s, '\0'));
+    // printf("%s\n\n", strchr(s, '\0'));
+    
+    // printf("%s\n", mx_strchr(s, 'C'));
+    // printf("%s\n\n", strchr(s, 'C'));
     
     // printf("%p\n", (void*)mx_strchr(NULL, 'g'));
     // printf("%p\n\n", (void*)strchr(NULL, 'g'));
