b06f38e7562fd65b66b675b6ded05ba9a77a1161
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-05 19:33:36 +0000

    fix atoi

--- sprint04/t03/mx_atoi.c
+++ sprint04/t03/mx_atoi.c
@@ -1,7 +1,32 @@
 #include <stdbool.h>
+// #include <stdlib.h>
+// #include <stdio.h>
 
 bool mx_isspace(char c);
 bool mx_isdigit(char c);
+
+
+// static char *itoa(long long n) {
+//     size_t size = 1;
+//     char *str = malloc(2);
+
+//     if (n == 0) {
+//         str[0] = '0';
+//         str[1] = '\0';
+//         return str;
+//     }
+//     if (n < 0) {
+//         str[0] = '-';
+//         size++;
+//     }
+//     for (long long n_copy = n; n_copy != 0; n_copy /= 10, size++) {}
+//     str = realloc(str, size);
+//     for (long long n_copy = n, i = size - 2; n_copy != 0; n_copy /= 10, i--)
+//         n_copy > 0 ? (str[i] = n_copy % 10 + 48) : (str[i] = ((n_copy % 10) * -1) + 48);
+//     str[size - 1] = '\0';
+//     return str;
+// }
+
 
 int mx_atoi(const char *str) {
     int result = 0, pos = 0;
@@ -15,14 +40,30 @@ int mx_atoi(const char *str) {
     return result;
 }
 
+
+
 // #include <stdio.h>
 
 // int main(void) {
-//     printf("%d\n", mx_atoi("  d -1"));
-//     printf("%d\n", mx_atoi("+ 18"));
-//     printf("%d\n", mx_atoi("   -10 99"));
-//     printf("%d\n", mx_atoi("   - 998"));
-//     printf("%d\n", mx_atoi("98 4"));
-//     printf("%d\n", mx_atoi("2147483647"));
-//     printf("%d\n", mx_atoi("-2147483648"));
+//     printf("%s\n", itoa(-2147483648));
+//     long long i = -9223372036854775807 -1;
+    
+//     long long k = -9223372036854775807 + -1;
+//     printf("%lld\n", k);
+//     int imp = 0, std = 0;
+    
+//     for (long long i = 0; k < i; i += (-2147483648 + -2147483648)) {
+//         imp = mx_atoi(itoa(i));
+//         std = atoi(itoa(i));
+//         if (imp != std) {
+//             printf("%lld --- error", i);
+//         }
+//     }
+    
+    // printf("%s = %d\n", itoa(i), mx_atoi(itoa(i)));
+    // printf("%s = %d\n", itoa(i), atoi(itoa(i)));
+    // printf("%d\n", atoi(""));
+    // printf("%d\n", atoi("ffff"));
+    // printf("%d\n", mx_atoi(""));
+    // printf("%d\n", mx_atoi("ffff"));
 // }
