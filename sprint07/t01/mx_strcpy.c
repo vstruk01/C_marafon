a0fa2393839fcc18c0ac94aacb89c6aa9713d2c9
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-06 21:14:19 +0000

    add mx_strdup

---
+++ sprint07/t01/mx_strcpy.c
@@ -0,0 +1,33 @@
+char *mx_strcpy(char *dst, char *str) {
+    int i = 0;
+    
+    for (;str[i]; dst[i] = str[i], ++i);
+    dst[i] = '\0';
+    return dst;
+}
+
+// #include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(void) {
+//     // strncpy(NULL, NULL, 10);
+//     // mx_strncpy(NULL, NULL, 10);
+    
+//     char *s = "the worst game on whole world";
+//     char *d = malloc(100);
+//     printf("%s\n", mx_strcpy(d, s));
+//     printf("%s\n", mx_strcpy(d, s));
+//     printf("%s\n", mx_strcpy(d, s));
+//     printf("%s\n", mx_strcpy(d, s));
+//     printf("%s\n", mx_strcpy(d, s));
+//     printf("%s\n\n", mx_strcpy(d, s));
+    
+//     char *d1 = malloc(100);
+//     printf("%s\n", strcpy(d1, s));
+//     printf("%s\n", strcpy(d1, s));
+//     printf("%s\n", strcpy(d1, s));
+//     printf("%s\n", strcpy(d1, s));
+//     printf("%s\n", strcpy(d1, s));
+//     printf("%s\n", strcpy(d1, s));
+// }
