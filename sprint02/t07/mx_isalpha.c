5d9916b2d4a1cea0a04483a2128a6fb88b2eb2ef
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-06 08:13:21 +0000

    comment main

--- sprint02/t07/mx_isalpha.c
+++ sprint02/t07/mx_isalpha.c
@@ -4,10 +4,10 @@ bool mx_isalpha(int c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ? true : false;
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void) {
-    for (char i = 32; i < 127; ++i)
-        printf("%c -> %s\n", i, mx_isalpha(i) ? "true" : "false");
-}
+// int main(void) {
+//     for (char i = 32; i < 127; ++i)
+//         printf("%c -> %s\n", i, mx_isalpha(i) ? "true" : "false");
+// }
 
