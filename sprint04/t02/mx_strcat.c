0c336312e769753170d5532e3f1cf67257ba5d00
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-07 13:37:07 +0000

    fix mx_strcat

--- sprint04/t02/mx_strcat.c
+++ sprint04/t02/mx_strcat.c
@@ -2,7 +2,6 @@ int mx_strlen(const char *s);
 
 char *mx_strcat(char *s1, const char *s2) {
     for (int i = mx_strlen(s1), j = 0; s2[j]; s1[i] = s2[j], ++j, ++i);
-    s1[mx_strlen(s1) + mx_strlen(s2)] = '\0';
     return s1;
 }
 
@@ -23,21 +22,19 @@ char *mx_strcat(char *s1, const char *s2) {
 //     char s2[20] = "";
 //     char s3[20] = "";
     
-//     printf("%lu\n%lu\n%lu\n", (long)s1, (long)s2, (long)s3);
+//     // printf("%lu\n%lu\n%lu\n", (long)s1, (long)s2, (long)s3);
     
-//     char *ss1 = "yo neo bro";
-//     char *ss2 = "yo neo bro";
-//     char *ss3 = "yo neo bro";
+//     char *ss1 = "yo neo broo";
+//     char *ss2 = "yo neo broo";
+//     char *ss3 = "yo neo broo";
 
 //     char *r1 = mx_strcat(s1, ss1);
 //     char *r2 = strcat(s2, ss2);
 //     char *r3 = my_strcat(s3, ss3);
     
-//     long l = (long)r1;
-    
-//     printf("mx  = (%s)\nlen = %lu\n\n", (void*)l, strlen(r1));
+//     printf("mx  = (%s)\nlen = %lu\n\n", r1, strlen(r1));
 //     printf("std = (%s)\nlen = %lu\n\n", r2, strlen(r2));
 //     printf("my  = (%s)\nlen = %lu\n\n", r3, strlen(r3));
 
-//     printf("%lu\n%lu\n%lu\n", (long)r1, (long)r2, (long)r3);
+//     // printf("%lu\n%lu\n%lu\n", (long)r1, (long)r2, (long)r3);
 // }
