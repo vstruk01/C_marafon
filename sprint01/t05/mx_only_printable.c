462621eadd252c89d99629675a162ebfc249e834
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-07 13:35:33 +0000

    fix mx_only_print_able

--- sprint01/t05/mx_only_printable.c
+++ sprint01/t05/mx_only_printable.c
@@ -1,7 +1,7 @@
 void mx_printchar(char c);
 
 void mx_only_printable(void) {
-    for (char i = 32; i < 127 && i >= 32; ++i)
+    for (char i = 126; i >= 32; --i)
         mx_printchar(i);
     mx_printchar('\n');
 }
