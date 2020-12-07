8e9dd28abe4fb40e9605f2fa2af8b1f2f6734a73
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-07 21:21:01 +0000

    choice

---
+++ sprint08/t01/choice.h
@@ -0,0 +1,17 @@
+#ifndef CHOICE_H
+#define CHOICE_H
+
+#include <stdio.h>
+
+#define MX_RED_PILL 1
+#define MX_BLUE_PILL 2
+
+#define MX_SUCCESS_PHRASE "Follow me!"
+#define MX_FAIL_PHRASE "Perhaps I was wrong about you, Neo."
+#define MX_UNDEFINED_PHRASE "Are you sure about that?"
+
+typedef char t_phrase;
+
+char *mx_strdup(const char *s);
+
+#endif
