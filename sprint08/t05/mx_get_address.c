477bd5f82d94ac9087a127508fd8b78ba97940f5
EC2 Default User <ec2-user@ip-172-31-33-51.eu-central-1.compute.internal>
2020-12-08 11:50:01 +0000

    fix get adress

--- sprint08/t05/mx_get_address.c
+++ sprint08/t05/mx_get_address.c
@@ -1,7 +1,7 @@
 #include "get_address.h"
 
 char *mx_get_address(void *p) {
-    char *address = mx_nbr_to_hex((long)p);
+    char *address = mx_nbr_to_hex((long)&p);
     char *result = mx_strnew(2 + mx_strlen(address));
     
     mx_strcpy(result, "0x");
@@ -17,8 +17,9 @@ char *mx_get_address(void *p) {
 // int main(void) {
 //     void *point = malloc(10);
     
-//     printf("point = %p\n", point);
+//     printf("point = %p\n", &point);
 //     printf("point = %s\n", mx_get_address(point));
+//     printf("point = %s\n", mx_get_address(&point));
 //     printf("null  = %s\n", mx_get_address(NULL));
 // }
 
