1b0f5deae1387c24b59a34b3b2ab108588e86f00
vstruk01 <stoliar444@gmail.com>
2020-12-08 20:40:43 +0000

    fix create_agent.h

--- sprint08/t02/create_agent.h
+++ sprint08/t02/create_agent.h
@@ -5,6 +5,7 @@
 
 #include "agent.h"
 
+t_agent *mx_create_agent(char *name, int power, int strength); 
 char *mx_strnew(const int size);
 int mx_strlen(const char *s);
 char *mx_strcpy(char *dst, char *str);
